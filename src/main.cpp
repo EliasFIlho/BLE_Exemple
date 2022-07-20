8c261f9f413ac7440a5f41875df50c76cba3383a
EliasFilho <eliasfilho202@gmail.com>
2022-07-20 12:50:18 -0300

    BLE + Acelerometro

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
+#include "Arduino_LSM9DS1.h"
 
 BLEService AccService("180A");
 BLEByteCharacteristic edgeCharacteristic("2A57", BLERead | BLEWrite);
 BLEStringCharacteristic percentCharacteristc("2A56",BLERead,5);
 
-void function01();
-void function02();
-void function03();
+void function(char LedC);
+
+float x, y, z;
+unsigned long previous_time = 0;
+unsigned long base_time = 10;  
+
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
@@ -47,13 +55,13 @@ void loop() {
       if(edgeCharacteristic.written()){
         switch (edgeCharacteristic.value()){
         case 01:
-          function01();
+          function('R');
           break;
         case 02:
-          function02();
+          function('G');
           break;
         case 03:
-          function03();
+          function('B');
           break;
         default:
           break;
@@ -65,27 +73,42 @@ void loop() {
 }
 
 
-void function01(){
-  Serial.println("Red LED on");
-  percentCharacteristc.setValue("RLO");
-  digitalWrite(LEDR, LOW);            
-  digitalWrite(LEDG, HIGH);         
-  digitalWrite(LEDB, HIGH);
-}
-
-void function02(){
-  Serial.println("Green LED on");
-  percentCharacteristc.setValue("GLO");
-  digitalWrite(LEDR, HIGH);            
-  digitalWrite(LEDG, LOW);         
-  digitalWrite(LEDB, HIGH);
-}
-
-void function03(){
-  Serial.println("Blue LED on");
-  percentCharacteristc.setValue("BLO");
-  digitalWrite(LEDR, HIGH);            
-  digitalWrite(LEDG, HIGH);         
-  digitalWrite(LEDB, LOW);
+void function(char LedC){
+  
+  switch (LedC)
+  {
+  case 'R':
+    Serial.println("Red LED on");
+    percentCharacteristc.setValue("RLO");
+    digitalWrite(LEDR, LOW);            
+    digitalWrite(LEDG, HIGH);         
+    digitalWrite(LEDB, HIGH);
+    break;
+  case 'G':
+    Serial.println("Green LED on");
+    percentCharacteristc.setValue("GLO");
+    digitalWrite(LEDR, HIGH);            
+    digitalWrite(LEDG, LOW);         
+    digitalWrite(LEDB, HIGH);
+    break;
+  case 'B':
+    Serial.println("Blue LED on");
+    percentCharacteristc.setValue("RLO");
+    digitalWrite(LEDR, HIGH);            
+    digitalWrite(LEDG, HIGH);         
+    digitalWrite(LEDB, LOW);
+    break;
+  default:
+    Serial.println("Unvalid value");
+    percentCharacteristc.setValue("NA");
+    break;
+  }
+  previous_time = millis();
+  while(millis()-previous_time <= 4000){
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(x, y, z);
+    }
+    Serial.println(String(x)+","+String(y)+","+String(z));
+  }
   
 }
